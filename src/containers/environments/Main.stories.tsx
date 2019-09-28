23889abeec0b2db547c6da8f91d27b2a5d85c033
igm50 <keisuke.igimi@gmail.com>
2019-09-28 21:24:10 +0900

    add: Mainコンポーネントの仮作成およびストーリー作成

---
+++ src/containers/environments/Main.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Main from './Main'
+
+export default {
+  title: 'Environment/Main'
+}
+
+export const Sample = () => <Main />
---
+++ src/containers/environments/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { AppBar, Toolbar } from '@material-ui/core'
+
+const Main: React.FC = () => {
+  return (
+    <React.Fragment>
+      <AppBar>
+        <Toolbar></Toolbar>
+      </AppBar>
+    </React.Fragment>
+  )
+}
+
+export default Main
