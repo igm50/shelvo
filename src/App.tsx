146c75c07ae53482bcc640879b2d5231f047bb81
igm50 <keisuke.igimi@gmail.com>
2019-09-30 20:33:24 +0900

    add: App.tsxからMain.tsxを読み込み

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './App.css'
 
+import Main from './environments/Main'
+
 const App: React.FC = () => {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  )
 }
 
 export default App
