acce1a219eb296075adcb6316b4051bbb98fdcbe
jintz <keisuke.igimi@gmail.com>
2019-10-05 10:08:03 +0900

    add: 本棚管理Organismを作成する

---
+++ src/containers/organisms/ManageShelves.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import ManageShelves from './ManageShelves'
+
+export default {
+  title: 'Organisms/ManageShelves'
+}
+
+export const Sample = () => {
+  return <ManageShelves />
+}
---
+++ src/containers/organisms/ManageShelves.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {} from '@material-ui/core'
+
+const ManageShelves: React.FC = () => {
+  return <div>本棚管理</div>
+}
+
+export default ManageShelves
