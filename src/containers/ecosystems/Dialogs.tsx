869a087e942a4f8d1d94aed4e145626396a490b5
jintz <keisuke.igimi@gmail.com>
2019-10-06 20:32:46 +0900

    update: ダイアログEcosystemに書籍登録Organismを表示

--- src/containers/ecosystems/Dialogs.tsx
+++ src/containers/ecosystems/Dialogs.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Dialog } from '@material-ui/core'
 
+import BookRegistrator from '../organisms/BookRegistrator'
+
 interface Props {
   isOpen: boolean
   close: () => void
@@ -9,7 +11,7 @@ interface Props {
 const Dialogs: React.FC<Props> = props => {
   return (
     <Dialog open={props.isOpen} onClose={props.close}>
-      <div>ダイアログ</div>
+      <BookRegistrator />
     </Dialog>
   )
 }
